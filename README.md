ad66b5bc445ebaf8d5f7f54fb4a5b6d96f0797bb
Stella Silvaroli <99274582+Stella-Silvaroli06@users.noreply.github.com>
2022-04-11 18:00:23 +0000

    Completed Unit3-02-HTML-VolumeTriangularPyramid

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Unit3-02-HTML-VolumeTriangularPyramid
+[![GitHub's Super Linter](https://github.com/ICS20-Programming-StellaS/Unit3-02-HTML-VolumeTriangularPyramid/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/ICS20-Programming-StellaS/Unit3-02-HTML-VolumeTriangularPyramid/actions)
---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2022 Stella S All rights reserved
+//
+// Created by: Stella S
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates the volume of a triangular pyramid
+ */
+function calculate () {
+  // input
+  let base = parseFloat(document.getElementById('base-of-triangular-pyramid').value)
+  
+  let height = parseFloat(document.getElementById('height-of-triangular-pyramid').value)
+  
+  let length = parseFloat(document.getElementById('length-of-triangular-pyramid').value)
+  
+  // process
+  const volume = (base * height * length)/2
+  
+  // output
+  document.getElementById('volume').innerHTML = 'The volume of the triangular pyramid is: ' + volume.toFixed(2) + ' cm²'
+}
